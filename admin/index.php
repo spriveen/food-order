d49618404fb603232fd8c6bad0fd959e20316855
spriveen <rpeanuka@gmail.com>
2025-04-02 08:07:58 -0700

    update admin.css

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,40 @@
 <html>
     <head>
         <title>Food Order website- Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
-    <h1>Admin Panel</h1>
+    <!-- Menu Section start -->
+     <div class="menu text-center"> 
+        <div class="class wrapper">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Admin</a></li>
+            <li><a href="#">Category</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Order</a></li>
+        </ul>
+        </div>
+        </div>
+     <!--  Menu Section End-->
+
+      <!-- Main Content Section start -->
+      <div class="main-content"> 
+      <div class="class wrapper">
+        Main Content Section Here
+        </div>
+    </div>
+     <!--  Main Content Section End-->
+
+     <!-- Footer Section start -->
+     <div class="footer"> 
+     <div class="class wrapper">
+        <p class="text-center">2025 All Right Reserved.Developed By<a href="#"> Riveen_Official </a></p>
+        
+        </div>
+    </div>
+     <!--  Footer Section End-->
 </body>
 </html>
