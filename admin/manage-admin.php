fe8ed1da167cf7cfebbb882662832174fcef8700
spriveen <rpeanuka@gmail.com>
2025-04-06 23:38:26 -0700

    update Admin.php

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,11 +1,67 @@
 <?php include('partials/menu.php'); ?>  
 
-      <!-- Main Content Section start -->
-      <div class="main-content"> 
-      <div class="class wrapper">
-       <h1>Manage Admin</h1>
-        </div>
-    </div>
-     <!--  Main Content Section End-->
-    <?php include('partials/footer.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/admin.css">
+    <title>Document</title>
+</head>
+<body>
     
+<!-- Main Content Section start -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+        
+    <br /><br />
+
+        <!-- Button to Add Admin -->
+         <a href="#" class="btn-primary">Add Admin</a>
+         </br><br /><br />
+        
+        
+         <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>User Name</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr >
+                <td>1.</td>
+                <td>Riveen Peanuka</td>
+                <td>riveenpeanuka</td>
+                <td >
+                <a href="#" class="btn-secondary">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+
+                <tr >
+                <td>2.</td>
+                <td>Riveen Peanuka</td>
+                <td>riveenpeanuka</td>
+                <td >
+                <a href="#" class="btn-secondary">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+            <tr >
+                <td>3.</td>
+                <td>Riveen Peanuka</td>
+                <td>riveenpeanuka</td>
+                <td >
+                <a href="#" class="btn-secondary">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+        </table>
+    </div>
+</div>
+<!-- Main Content Section End-->
+
+<?php include('partials/footer.php'); ?>
+
+</body>
+</html>
--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -15,6 +15,8 @@
             <li><a href="manage-category.php">Category</a></li>
             <li><a href="manage-food.php">Food</a></li>
             <li><a href="manage-order.php">Order</a></li>
+
+            
         </ul>
         </div>
         </div>
