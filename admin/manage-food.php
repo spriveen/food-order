a79d9ea91346f7c008fa7d62878b0caab61e80f2
spriveen <rpeanuka@gmail.com>
2025-04-06 23:43:42 -0700

    copy the path

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -3,6 +3,48 @@
 <div class="min-content">
    <div class="wrapper">
    <h1>Manage Food</h1>
+   <br /><br />
+
+<!-- Button to Add Admin -->
+ <a href="#" class="btn-primary">Add Food</a>
+ </br><br /><br />
+
+
+ <table class="tbl-full">
+    <tr>
+        <th>S.N</th>
+        <th>Full Name</th>
+        <th>User Name</th>
+        <th>Actions</th>
+    </tr>
+
+    <tr >
+        <td>1.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+
+        <tr >
+        <td>2.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+    </tr>
+    <tr >
+        <td>3.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+</table>
    </div> 
 
 </div>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,6 +4,46 @@
       <div class="main-content"> 
       <div class="class wrapper">
        <h1>Manage Order</h1>
+       
+
+    </br><br /><br />
+
+
+ <table class="tbl-full">
+    <tr>
+        <th>S.N</th>
+        <th>Full Name</th>
+        <th>User Name</th>
+        <th>Actions</th>
+    </tr>
+
+    <tr >
+        <td>1.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+
+        <tr >
+        <td>2.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+    </tr>
+    <tr >
+        <td>3.</td>
+        <td>Riveen Peanuka</td>
+        <td>riveenpeanuka</td>
+        <td >
+        <a href="#" class="btn-secondary">Update Admin</a>
+        <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+</table>
         </div>
     </div>
      <!--  Main Content Section End-->
